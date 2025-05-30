2d518732d33286f2e02293d20240275a140fa0d1
Owen Henry <61534213+WolfgangSuperman@users.noreply.github.com>
2025-05-30 13:50:12 -0400

    Revert "Added function patches and additions"

--- src/nova_times/cli.py
+++ src/nova_times/cli.py
@@ -8,7 +8,6 @@ from astropy.table import Table
 
 from nova_times.describe import describe_dataset
 from nova_times.measure import measure_time
-from nova_times.measure import measure_tN
 from nova_times.viz import viz_dataset
 from nova_times.io import read_csv
 
@@ -56,27 +55,14 @@ def viz(filename: str, output_filename: str, band: Optional[str] = None) -> None
 @click.argument("filename", required=True)
 # @click.argument("output_filename", required=True)
 # @click.option("-b", "--band", "band")
-def measure_times(filename: str) -> None:
+def measure(filename: str) -> None:
     data_table = read_file(filename)
 
     timing_data = measure_time(data_table)
 
     print(json.dumps(timing_data, indent=4))
 
-@click.command()
-@click.argument("filename", required=True)
-# @click.argument("output_filename", required=True)
-# @click.option("-b", "--band", "band")
-def measure_tNs(filename: str) -> None:
-    data_table = read_file(filename)
-
-    timing_data = measure_tN(data_table)
-
-    print(json.dumps(timing_data, indent=4))    
-
 
 cli.add_command(describe)
 cli.add_command(viz)
-cli.add_command(measure_times)
-cli.add_command(measure_tNs)
-
+cli.add_command(measure)
--- src/nova_times/measure.py
+++ src/nova_times/measure.py
@@ -2,9 +2,6 @@ from typing import TypedDict
 
 import numpy as np
 from astropy.table import Table
-import pandas as pd
-
-from sklearn.ensemble import GradientBoostingRegressor
 
 
 TimingData = TypedDict(
@@ -18,124 +15,29 @@ TimingData = TypedDict(
     },
 )
 
-MagData = TypedDict(
-    "MagData",
-    {
-        "band": str,
-        "predicted_mag": float,
-        "eruption_time": float,
-        "time_from_eruption": float,
-        "tN_jd": float,
-    },
-)
 
 def measure_time(dataset: Table) -> TimingData:
-    
     mask = dataset.groups.keys["Band"] == "V"
-    singlebanddata = dataset.groups[mask]
-    magnitudes = singlebanddata["Magnitude"]
-    jds = singlebanddata["JD"]
-    
-    alldata = dataset
-    
+    singleband_data = dataset.groups[mask]
+    magnitudes = singleband_data["Magnitude"]
+    jds = singleband_data["JD"]
+
+    # maximum
     maximum_mag = min(magnitudes)
     maximum_indx = np.argmin(magnitudes)
     maximum_jd = jds[maximum_indx]
-    
-    X = np.array(jds)
-    y = np.array(magnitudes)
-    #print(len(X), len(y))
-    
-    X = X[~np.isnan(y)]
-    y = y[~np.isnan(y)]
-    #print(len(X), len(y))
-    
-    X = X[np.argmin(y):]
-    y = y[np.argmin(y):]
-    X =  X.reshape(-1, 1)
-    
-    #X_all = np.array(alldata['MET'])
-    X_all = np.array(alldata['JD'][alldata['JD']<max(singlebanddata['JD'])])
-    X_all = np.asarray(sorted(X_all[np.argmin(y):]))
-    X_all = X_all.reshape(-1, 1)
-    
-    #have now included a data quality flag, if the LC is particualry data poor (or you are using binned means) the max_depth is increased
-    if len(singlebanddata) < 100: 
-        gbm = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, max_depth=5)
-    else:
-        gbm = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, max_depth=3)
-        
-    gbm.fit(X, y)
-    
-    fit = gbm.predict(X_all)
-    
-    t2_indx = np.argmin(np.abs(fit - (y.min()+2)))
-    #print(t2_indx)
-    #tN_mag = y[tN_indx]
-    t2_mag = fit[t2_indx]
-    
-    t2_jd = X_all[t2_indx]
-    
-    #print(maximum_jd,maximum_mag,t2_mag,t2_jd[0])
-    #print(type(maximum_jd), type(maximum_mag), type(t2_mag), type(t2_jd[0]))
-    
+
+    # T2
+    t2_mag_calc = maximum_mag + 2
+    t2_indx = np.argmin(np.abs(magnitudes - t2_mag_calc))
+    t2_mag = magnitudes[t2_indx]
+    t2_jd = jds[t2_indx]
+
     results = TimingData(
         band="V",
         maximum_jd=maximum_jd,
         maximum_mag=maximum_mag,
         t2_mag=t2_mag,
-        t2_jd=t2_jd[0],
+        t2_jd=t2_jd,
     )
-    
-    return(results)
-
-def measure_tN(dataset: Table) -> TimingData:
-    
-    window = input('What is your window size (measure in days from eruption peak):')
-    window = float(window)*86400
-    
-    mask = dataset.groups.keys["Band"] == "V"
-    singlebanddata = dataset.groups[mask]
-    magnitudes = singlebanddata["Magnitude"]
-    jds = singlebanddata["JD"]
-    
-    alldata = dataset
-    
-    maximum_mag = min(magnitudes)
-    maximum_indx = np.argmin(magnitudes)
-    maximum_jd = jds[maximum_indx]
-    
-    X = np.array(jds)
-    y = np.array(magnitudes)
-    
-    X = X[~np.isnan(y)]
-    y = y[~np.isnan(y)]
-    #X = np.array(data['MET']) #parsing the dataframe
-    #y = np.array(data['Magnitude'])
-    
-    X =  X.reshape(-1, 1) #columnating the vector
-    
-    gbm = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, max_depth=1) #standard GBM
-    gbm.fit(X, y)
-    
-    #fit = gbm.predict(X) #just passing X data right back will reproduce original data exactly 
-
-    preds = np.array([X[np.argmin(y)]+window]) #predict magnitude according to fit a window away from the LC maximum
-    preds = preds.reshape(-1, 1)
-    predictions = gbm.predict(preds)
-    #print(type(predictions))
-    tN = (predictions[0] - y.min()) #tN is defined as the amount of time it takes the LC magnitude to reduce by N, this is the amount the LC has diminished
-    #print(tN)
-    
-    #print(predictions[0],X[np.argmin(y)],window,tN)
-    #print(type(predictions[0]), type(X[np.argmin(y)]), type(window), type(tN))
-    
-    results = MagData(
-        band="V",
-        predicted_mag=predictions[0],
-        eruption_time=X[np.argmin(y)][0],
-        time_from_eruption=window,
-        tN_jd=tN,
-    )
-    
-    return(results)
+    return results
