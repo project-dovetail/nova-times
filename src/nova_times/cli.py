ef27d35fa6fff81a65e10640780d8da22523fc47
Owen Henry <61534213+WolfgangSuperman@users.noreply.github.com>
2025-05-30 13:35:31 -0400

    Add files via upload

--- src/nova_times/cli.py
+++ src/nova_times/cli.py
@@ -8,6 +8,7 @@ from astropy.table import Table
 
 from nova_times.describe import describe_dataset
 from nova_times.measure import measure_time
+from nova_times.measure import measure_tN
 from nova_times.viz import viz_dataset
 from nova_times.io import read_csv
 
@@ -55,14 +56,27 @@ def viz(filename: str, output_filename: str, band: Optional[str] = None) -> None
 @click.argument("filename", required=True)
 # @click.argument("output_filename", required=True)
 # @click.option("-b", "--band", "band")
-def measure(filename: str) -> None:
+def measure_times(filename: str) -> None:
     data_table = read_file(filename)
 
     timing_data = measure_time(data_table)
 
     print(json.dumps(timing_data, indent=4))
 
+@click.command()
+@click.argument("filename", required=True)
+# @click.argument("output_filename", required=True)
+# @click.option("-b", "--band", "band")
+def measure_tNs(filename: str) -> None:
+    data_table = read_file(filename)
+
+    timing_data = measure_tN(data_table)
+
+    print(json.dumps(timing_data, indent=4))    
+
 
 cli.add_command(describe)
 cli.add_command(viz)
-cli.add_command(measure)
+cli.add_command(measure_times)
+cli.add_command(measure_tNs)
+
